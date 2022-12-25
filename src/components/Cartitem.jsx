f5e8df50c4e88f3006b5604fe55b1e3d8168c953
Yash Mittal <yashmittal413@gmail.com>
2022-12-25 18:38:09 +0530

    used state and add increasequantity func

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Cartitem = () => {
-  return (
-    <div className='cartitem'>
-      <div className="left-block">
-        <img alt='sample' style={styles.image} />
-      </div>
-      <div className="right-block">
-        <div style={ { fontSize: 45 } }>Phone</div>
-        <div style={{ color: '#777' }}>Rs 14000</div>
-        <div style={{ color: '#777' }}>Qty: 1</div>
-        <div className="cart-action">
-          {/* Button */}
+export class Cartitem extends Component {
+  
+    constructor () {
+        super();
+        this.state ={
+            title: 'Mobile Phone',
+            price: 1400,
+            qty: 1,
+            img: ''
+        }
+    }
+    increaseQuantity = () => {
+      console.log('this',this.state);
+    }
+
+  render() {
+    const { price, title, qty }=this.state;
+    return (
+        <div className='cartitem'>
+        <div className="left-block">
+          <img alt='' className="images" />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={{ color: '#777' }}>Rs {price}</div>
+          <div style={{ color: '#777' }}>Qty: {qty}</div>
+          <div className="cart-action">
+            {/* Button */}
+            <img src="https://cdn-icons-png.flaticon.com/512/1665/1665680.png" className="icons" alt="increase" onClick={this.increaseQuantity} />
+            <img src="https://cdn-icons-png.flaticon.com/512/1665/1665714.png" className="icons" alt="decrease" />
+            <img src="https://cdn-icons-png.flaticon.com/512/3221/3221897.png" className="icons" alt="delete" />
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
-
-const styles = {
-  image: {
-    height: 110,
-    width: 110,
-    borderRadius: 4,
-    background: '#ccc'
+    )
   }
 }
 
